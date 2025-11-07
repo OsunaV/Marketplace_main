c079ae091767b06009e23130cd178b2a83ab227e
OsunaV <jose.valdez.osuna.08@gmail.com>
2025-11-07 11:05:26 -0800

    Se arreglaron errores varios y se agrego la pestaña de detallas

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,5 @@ from .views import contact, detail
 
 urlpatterns = [
     path('contact/', contact, name='contact'),
-    path('details/<int:pk>/', detail, name='detail')
+    path('detail/<int:pk>/', detail, name='detail')
     ]
